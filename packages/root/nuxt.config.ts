f14c4d829a2c79810cb46be6bd4e505d24a03b6b
undancer <undancer+github@gmail.com>
2024-08-25 13:27:51 +0800

    初始化提交

--- packages/root/nuxt.config.ts
+++ packages/root/nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+  ],
 })
